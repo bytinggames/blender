f0601e6ddcba336f3ddbe029851bb23ad10616a4
bytinggames <bytinggames@gmail.com>
2021-05-06 14:13:34 +0200

    used predefined math functions

--- source/blender/modifiers/intern/MOD_quarterpipe.c
+++ source/blender/modifiers/intern/MOD_quarterpipe.c
@@ -35,46 +35,8 @@
 #include <math.h>
 #include <stdlib.h>
 
-const float PI = 3.1415926535897932384626433832795;
+#include "BLI_math.h"
 
-static void setCoord(float co[], float x, float y, float z)
-{
-  co[0] = x;
-  co[1] = y;
-  co[2] = z;
-}
-static void setCoord2(float coTarget[], float coSource[])
-{
-  coTarget[0] = coSource[0];
-  coTarget[1] = coSource[1];
-  coTarget[2] = coSource[2];
-}
-
-static float *subtract(float from[], float amount[])
-{
-  float *value = malloc(3 * sizeof(float));
-  value[0] = from[0] - amount[0];
-  value[1] = from[1] - amount[1];
-  value[2] = from[2] - amount[2];
-  return value;
-}
-
-static float vLength(float v[])
-{
-  return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-}
-static float vLength2(float v[])
-{
-  return sqrt(v[0] * v[0] + v[1] * v[1]);
-}
-static float *vNormalize2(float v[])
-{
-  float length = vLength2(v);
-  float *out = malloc(2 * sizeof(float));
-  out[0] = v[0] / length;
-  out[1] = v[1] / length;
-  return out;
-}
 
 static Mesh *modifyMesh(struct ModifierData *md,
                         const struct ModifierEvalContext *ctx,
@@ -97,27 +59,29 @@ static Mesh *modifyMesh(struct ModifierData *md,
 
   const int faceVs = 4;
 
-  float anglePlus = PI / 2.f / steps;
+  float anglePlus = M_PI_2 / steps;
   float angle = anglePlus;
   MVert *top = (MVert[]){origin[0], origin[2]};
 
   float size = 1.f;
   float height[2];
   float *pipeStart[2] = {origin[0].co, origin[2].co};
-  float *lengthDir = subtract(pipeStart[1], pipeStart[0]);
+  float lengthDir[3];
+  sub_v3_v3v3(lengthDir, pipeStart[1], pipeStart[0]);
   float heightDir[2][3];  // points upwards
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 3; j++) {
       heightDir[i][j] = origin[i * 2].co[j] - origin[i * 2 + 1].co[j];
     }
-    height[i] = vLength(heightDir[i]);
+    height[i] = len_v3(heightDir[i]);
   }
 
   float widthFactor = 1.f;
   float width[] = {height[0] * widthFactor, height[1] * widthFactor};  // _
 
   float lengthDir2D[2] = {lengthDir[0], lengthDir[1]};
-  float *lengthDir2D_1N = vNormalize2((float[]){-lengthDir[1], lengthDir[0]});
+  float lengthDir2D_1N[2] = {-lengthDir[1], lengthDir[0]};
+  normalize_v2(lengthDir2D_1N);
   float widthDir[2][3];  // points in width direction
   for (int i = 0; i < 2; i++) {
     widthDir[i][0] = lengthDir2D_1N[0] * width[i];
@@ -134,9 +98,8 @@ static Mesh *modifyMesh(struct ModifierData *md,
 
   // TODO: fix rotation on object
 
-
-  setCoord2(mvert[0].co, pipeStart[0]);  // top left
-  setCoord2(mvert[1].co, pipeStart[1]);  // top right
+  copy_v3_v3(mvert[0].co, pipeStart[0]);
+  copy_v3_v3(mvert[1].co, pipeStart[1]);
 
   mvert += 2;
   for (int step = 0; step < steps; step++, angle += anglePlus, mvert += 2, mloop += faceVs) {
